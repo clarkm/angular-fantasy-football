39ea1176706813bb240aed4a06edf4b3c416a95f
clark <clark@Macintosh.local>
2016-08-01 12:01:55 -0400

    scraper team, bye, name, rank, pos working! (for fantasypros)

--- scraper.py
+++ scraper.py
@@ -11,19 +11,23 @@ file = open('output.json','w')
 playerList = []
 
 table = soup.find_all('tbody')[1]
-#tbody = table.find('tbody')
+
 for tag in table.find_all('tr'):
     player = {}
     data = tag.find_all('td')
+    team = tag.find_all('small')
 
     if data[0]:
         player['rank'] = data[0].text
     if len(data) > 1:
-        player['name'] = data[1].text
+        player['name'] = data[1].find_all('a')[0].text
     if len(data) > 2:
-        player['team'] = data[2].text
+        player['pos'] = data[2].text
     if len(data) > 3:
         player['bye'] = data[3].text
+    if len(data) > 1:
+        if data[1].find_all('small'):
+            player['team'] = data[1].find_all('small')[0].text
 
 
 
