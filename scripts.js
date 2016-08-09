5753e10c2444a1d3925f3442d45774beb256f3fe
clark <clark@Macintosh.local>
2016-08-09 16:48:46 -0400

    disable team creator when team length >= 12

--- scripts.js
+++ scripts.js
@@ -25,6 +25,7 @@ app.controller('appController', ['myService', '$scope','$http',
         vm.sortReverse = false;
         vm.searchPlayers = '';
         vm.addActive = false;
+        vm.disableTeamCreator = false;
 
         vm.selectedTeam = null;
         vm.players = [];
@@ -44,21 +45,25 @@ app.controller('appController', ['myService', '$scope','$http',
             /* make a function so when a row is clicked, index2 becomes that row's 'absolute'/original index.
             then the addItem function will add it correctly to the right team. */
 
-
             vm.index2 = 0;
-
 
         vm.teams = [];
         vm.addTeam = function (teamString) {
-            vm.addActive = true;
-            console.log('addActive ' + vm.addActive);
-            console.log('selected team: ' + vm.selectedTeam);
-            vm.teams.push({
-              selected: false,
-              name: teamString,
-              players: []
-            });
-            vm.selectedTeam = vm.teams[vm.index2];
+          if (vm.teams.length >= 12) {
+            vm.disableTeamCreator = true;
+            //maybe display a md-toast popup here alerting that the teams are full
+          }
+          if (!vm.disableTeamCreator) {
+              vm.addActive = true;
+              console.log('addActive ' + vm.addActive);
+              console.log('selected team: ' + vm.selectedTeam);
+              vm.teams.push({
+                selected: false,
+                name: teamString,
+                players: []
+              });
+              vm.selectedTeam = vm.teams[vm.index2];
+            }
         };
 
         vm.updateSelectedTeam = function (team){
@@ -84,29 +89,29 @@ app.controller('appController', ['myService', '$scope','$http',
 
         vm.addPlayer = function(index, item){
           if (vm.addActive) {
-            vm.players.splice(index, 1);
+              vm.players.splice(index, 1);
 
-            if (vm.selectedTeam !== null){
+              if (vm.selectedTeam !== null){
 
-                // checks to see if it's the last team.....
-                if (vm.index2 > vm.teams.length - 1) {
-                    console.log('going back to 0');
-                    vm.selectedTeam = vm.teams[vm.index2];
-                }
+                  // checks to see if it's the last team.....
+                  if (vm.index2 > vm.teams.length - 1) {
+                      console.log('going back to 0');
+                      vm.selectedTeam = vm.teams[vm.index2];
+                  }
 
-                vm.selectedTeam.players.push(item);
-                vm.index2 += 1;
-                if (vm.index2 % vm.teams.length === 0) {
-                  vm.index2 = 0;
-                }
-                vm.selectedTeam = vm.teams[vm.index2];
-                return;
-            }
+                  vm.selectedTeam.players.push(item);
+                  vm.index2 += 1;
+                  if (vm.index2 % vm.teams.length === 0) {
+                    vm.index2 = 0;
+                  }
+                  vm.selectedTeam = vm.teams[vm.index2];
+                  return;
+              }
 
-            if (vm.index2 === 0) {
-              console('never hits this');
-              vm.teams[0].addClass('selected');
-            }
+              if (vm.index2 === 0) {
+                console('never hits this');
+                vm.teams[0].addClass('selected');
+              }
 
             // onclick, vm.index = index2.index
             vm.teams[vm.index2].players.push(item);
